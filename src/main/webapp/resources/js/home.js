895b9e19ca23d9afc04c422a315c176605ad9fd7
Asura <skmail90@inbox.ru>
2016-09-01 17:56:01 +0300

    minor fix

--- src/main/webapp/resources/js/home.js
+++ src/main/webapp/resources/js/home.js
@@ -1,10 +1,24 @@
 /**
  * Created by Asura on 31.08.2016.
  */
+/*
 window.onscroll = function() {
     if (window.pageYOffset > 100)
         document.getElementById('topHideButt').style.display = 'block';
     else
         document.getElementById('topHideButt').style.display = 'none';
-}
+}*/
 //http://easywebscripts.net/javascript/opacity.php
+
+/*
+ $('img', '#jq_op')
+ .each(function (){
+ $(this).data('op',$(this).css('opacity'));
+ })
+ .mouseover(function (){
+ $(this).animate({opacity: 1}, 500 );
+ })
+ .mouseout(function (){
+ $(this).stop(true,true).animate({opacity: $(this).data('op')}, 500 );
+ });
+ */
